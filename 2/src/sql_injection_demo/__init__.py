c433d83d972221f9e000c6f75cd63a9d64304383
Nathan Chappell <nchappell@mono.software>
2023-03-24 10:49:25 +0100

    sql injection

--- 2/src/sql_injection_demo/__init__.py
+++ 2/src/sql_injection_demo/__init__.py
@@ -1,5 +1,14 @@
 """
 Ensure proper importing and configuration
+
+you can hit an endpoint from powershell like:
+
+# normal request
+Invoke-RestMethod 127.0.0.1:8000/get-secrets -Method Post -Body '{"username":"user1", "password":"password1"}'
+
+# sql injection
+Invoke-RestMethod 127.0.0.1:8000/get-secrets -Method Post -Body '{"username":"'' or true --", "password":"password1"}'
+
 """
 
 import logging
